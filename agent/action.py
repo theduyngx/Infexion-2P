eb8ea043049214e0efee301c4a9164dbe2a95b8c
theduynguyen <theduynguyen2001@gmail.com>
2023-04-27 03:31:32 +1000

    action: evaluation function init

---
+++ agent/action.py
@@ -0,0 +1,11 @@
+# evaluation function
+from agent import Agent
+from referee.game import PlayerColor
+from .board import Board
+
+
+def evaluate(board: Board, agent: Agent) -> float:
+    # for now let's only consider Material
+    player_color: PlayerColor = agent.get_color()
+    power_diff = board.num_players(player_color) - board.num_players(player_color.opponent)
+    return 0
