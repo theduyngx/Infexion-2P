eca98aeae3f64fd5aa81285ac04e7152118a9eeb
theduynguyen <theduynguyen2001@gmail.com>
2023-04-27 01:00:23 +1000

    Agent's board init

--- agent/program.py
+++ agent/program.py
@@ -1,15 +1,16 @@
 # COMP30024 Artificial Intelligence, Semester 1 2023
 # Project Part B: Game Playing Agent
+from agent.board import Board
 from referee.game import PlayerColor, Action, SpawnAction, SpreadAction, HexPos, HexDir
 
 
-# This is the entry point for your game playing agent. Currently, the agent
-# simply spawns a token at the centre of the board if playing as RED, and
-# spreads a token at the centre of the board if playing as BLUE. This is
-# intended to serve as an example of how to use the referee API -- obviously
-# this is not a valid strategy for actually playing the game!
+board: Board = Board()
 
-board: dict = {}
+
+def print_referee(referee: dict):
+    print("-------------------------------")
+    print("Time remaining  :", referee["time_remaining"])
+    print("Space remaining :", referee["space_remaining"])
 
 
 class Agent:
@@ -39,10 +40,10 @@ class Agent:
         """
         Update the agent with the last player's action.
         """
-        print("-------------------------------")
-        print("Time remaining  :", referee["time_remaining"])
-        print("Space remaining :", referee["space_remaining"])
         assert self
+        print_referee(referee)
+        board.apply_action(action, concrete=True)
+
         match action:
             case SpawnAction(cell):
                 print(f"Testing: {color} SPAWN at {cell}")
