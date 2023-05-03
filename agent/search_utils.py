1c07d2ff5407f976418ffbb01e64a0b0f3f6ee4d
theduynguyen <theduynguyen2001@gmail.com>
2023-05-03 23:30:02 +1000

    get_legal_moves: optimized endgame play

--- agent/search_utils.py
+++ agent/search_utils.py
@@ -10,8 +10,11 @@ to ignore specific actions that are considered 'quiet', viz. not having signific
 """
 
 from agent.board import Board
-from agent.constants import MIN_TOTAL_POWER
-from referee.game import HexPos, HexDir, PlayerColor, Action, SpawnAction, MAX_TOTAL_POWER, SpreadAction
+from agent.constants import MIN_TOTAL_POWER, EMPTY_POWER
+from referee.game import HexPos, HexDir, PlayerColor, Action, SpawnAction, MAX_TOTAL_POWER, SpreadAction, BOARD_N
+
+# Constant
+MAX_ENDGAME_NUM_OPPONENT: int = 2
 
 
 def assert_action(action):
@@ -39,32 +42,71 @@ def adjacent_positions(pos: HexPos) -> list[HexPos]:
     return [pos + dir for dir in HexDir]
 
 
+def check_endgame(board: Board, color: PlayerColor) -> (list[Action], int, int):
+    """
+    Endgame detection - the optimization function for getting all legal nodes on the condition
+    that the game is reaching its end.
+    @param board : the board
+    @param color : player's color
+    @return      : the list of actions for endgame (if list is empty then not endgame),
+                   the player's total power (for reusing),
+                   the opponent's total power (for reusing)
+    """
+    # list of actions on the condition that it has reached endgame
+    actions: list[Action] = []
+    player_num, player_power = board.color_number_and_power(color)
+    opponent_num, opponent_power = board.color_number_and_power(color.opponent)
+
+    # endgame conditions: minimal player power requirement and number of opponents
+    if player_power > MAX_TOTAL_POWER / 2 and opponent_num <= MAX_ENDGAME_NUM_OPPONENT:
+        opponents = board.player_cells(color.opponent)
+
+        # endgame condition - at least 1 opponent piece is of single stack power
+        if any([opponent.power == 1 for opponent in opponents]):
+            for opponent in opponents:
+                # for each direction, get the same direction ranges
+                for dir in HexDir:
+                    ranges = [range(1, BOARD_N//2 + 1), range(-1, -BOARD_N//2 - 1, -1)]
+                    for r in ranges:
+
+                        # for each cell in said direction
+                        for s in r:
+                            curr_pos = opponent.pos + (dir * s)
+                            cell = board[curr_pos]
+                            # make sure that it is not an empty cell or opponent's cell
+                            if cell.power == EMPTY_POWER or cell.color == color.opponent:
+                                continue
+                            # append to actions if cell can reach the opponent
+                            if cell.power >= abs(s):
+                                actions.append(SpreadAction(curr_pos, dir))
+
+            # # if found actions, return immediately
+            # if len(actions) > 0:
+            #     print("\nFOUND")
+            #     for action in actions:
+            #         print(action)
+            #     print()
+    return actions, player_power, opponent_power
+
+
 def get_legal_moves(board: Board, color: PlayerColor, full=True) -> list[Action]:
     """
     Get all possible legal moves of a specified player color from a specific state of the board.
-
-    NOTE: add endgame detection here
-    Pseudocode:
-    if player_power > MAX_TOTAL_POWER / 2 and num_opponent <= 2:
-        if there exists an opponent power == 1 (meaning at most 1 opponent with power above 1):
-            if opponent_power > 2:
-                for all red pieces:
-                    if red.action can eat opponent:
-                        actions.append(action)
-            else:
-                for both power-1 blue pieces:
-                    if red.action can eat opponent:
-                        actions.append(action)
-
+    There are several optimizations made for this function in order reduce the number of legal
+    moves had to be generated in the minimax tree. This includes endgame detection and ignoring
+    specific moves based on domain knowledge of the game.
     @param board : specified board
     @param color : specified player's color
     @param full  : to get the full list of legal moves if true, or reduced list if otherwise
     @return      : list of all actions that could be applied to board
     """
+
+    # endgame check
+    actions, player_power, opponent_power = check_endgame(board, color)
+    if len(actions) > 0:
+        return actions
+
     # for every possible move from a given board state, including SPAWN and SPREAD
-    actions: list[Action] = []
-    player_power   = board.color_power(color)
-    opponent_power = board.color_power(color.opponent)
     for cell in board.get_cells():
 
         # append spawn actions - if full then always append (if power < 49), otherwise append on condition
