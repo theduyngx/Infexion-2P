4e12dc455977ce6c7ee75de65e8328cbede92749
Raja Felipe <rfelipe@student.unimelb.edu.au>
2023-05-04 01:15:18 +1000

    MC_NODE: Debugged implementation only using actions

--- agent/mc_node.py
+++ agent/mc_node.py
@@ -5,6 +5,13 @@ from .evaluation import evaluate
 from .agent_test import random_move
 from referee.game import PlayerColor, Action
 from .minimax import minimax
+from agent.constants import DEPTH
+
+UCT_CONSTANT: int = sqrt(2)
+RED_VICTORY: int = 1
+BLUE_VICTORY: int = -1
+LOWER_BOUND: int = -49
+UPPER_BOUND: int = 49
 
 
 class MonteCarloNode:
@@ -16,14 +23,11 @@ class MonteCarloNode:
         "visited",
         "action",
         "parent",
-        "children"
+        "children",
+        "hash_val"
     ]
 
-    UCT_CONSTANT: int = sqrt(2)
-    RED_VICTORY: int = 1
-    BLUE_VICTORY: int = -1
-
-    def __init__(self, action: Action, parent=None):
+    def __init__(self, action: Action, board: Board, parent=None):
         self.value: int = 0
         self.victory: int = 0
         self.uct: float = 0
@@ -40,19 +44,28 @@ class MonteCarloNode:
         # Need this when recalculating UCT
         # TODO: better data structure to store children (LinkedList?)
         self.children: list[MonteCarloNode] = []
+        self.hash_val = self.__hash__(board)
         return
 
     # Use this to calculate the evaluation value
     # of the node
     def evaluate_node(self, board: Board):
-        self.evaluation = evaluate(board)
+        _, self.evaluation = evaluate(board)
         return
 
     # Formula for UCT Calculation, tries to find a
     # balance between exploitation and exploration
     def calculate_uct(self):
         self.uct = self.value + \
-                   MonteCarloNode.UCT_CONSTANT*sqrt((log(self.visited)/log(self.parent.visited)))
+                   UCT_CONSTANT*sqrt((log(self.visited)/log(self.parent.visited)))
+        return
+
+    # Modified uct_value that does not need to simulate?
+    def calculate_new_uct(self):
+        # Normalize the value first; should be [0, 1]
+        std_eval = (self.evaluation-LOWER_BOUND)/(UPPER_BOUND-LOWER_BOUND)
+        self.uct = std_eval + \
+            UCT_CONSTANT*sqrt((log(self.visited)/log(self.parent.visited)))
         return
 
     # Each node should have a back propagation function
@@ -63,28 +76,42 @@ class MonteCarloNode:
         while curr_node is not None:
             # Since we're only referring to one board,
             # need to undo the action of the board each time
-            board = board.undo_action()
+            board.undo_action()
             curr_node.value += added_value
             # Need to update the UCTs of the children
             for child in curr_node.children:
                 child.calculate_uct()
-            curr_node = self.parent
+            curr_node = curr_node.parent
+
+    def quick_back_propagate(self, board: Board):
+        curr_node: MonteCarloNode = self
+        added_value: int = self.victory
+        while curr_node is not None:
+            # Since we're only referring to one board,
+            # need to undo the action of the board each time
+            board.undo_action()
+            curr_node.value += added_value
+            curr_node.visited += 1
+            # Need to update the UCTs of the children
+            for child in curr_node.children:
+                child.calculate_new_uct()
+            curr_node = curr_node.parent
 
     # LIGHT-SIMULATION: Randomly pick moves until
     # a goal state is reached -> REALLY SLOW
     # TODO: Try achieve a sub-goal instead of a real goal
     def light_simulate(self):
         test_board: Board = copy.deepcopy(self.board)
-        while not test_board.game_over():
+        while not test_board.game_over:
             new_action = random_move(test_board, test_board.turn_color)
-            test_board = test_board.apply_action(new_action)
+            test_board.apply_action(new_action)
         # Now you have to check which player won
         if test_board.winner_color is None:
             return
         elif test_board.winner_color == PlayerColor.RED:
-            self.victory = MonteCarloNode.RED_VICTORY
+            self.victory = RED_VICTORY
         else:
-            self.victory = MonteCarloNode.BLUE_VICTORY
+            self.victory = BLUE_VICTORY
 
     # HARD-SIMULATION: Use heuristics instead to keep
     # implementing moves until a goal state is reached
@@ -92,9 +119,9 @@ class MonteCarloNode:
         # TODO: Implement the game playing function
         #  using the evaluation function
         num_moves: int = 0
-        while not board.game_over():
-            new_action: Action = minimax(board, board.turn_color)
-            board = board.apply_action(new_action)
+        while not board.game_over:
+            new_action: Action = minimax(board, DEPTH, board.turn_color)
+            board.apply_action(new_action)
             num_moves += 1
 
         # Make sure to revert the board back
@@ -108,6 +135,12 @@ class MonteCarloNode:
         else:
             self.victory = MonteCarloNode.BLUE_VICTORY
 
+    # This simulation really only returns the new value,
+    # although normalized to be in range [0, 1]
+    def quick_simulate(self):
+        self.calculate_new_uct()
+        return
+
     # When picking a node to go to, always
     # pick the node with the lower evaluation value
     def __lt__(self, other):
@@ -118,6 +151,3 @@ class MonteCarloNode:
 
     def __hash__(self, board):
         return hash(frozenset(board.get_cells()))
-
-    def __iter__(self):
-        yield
