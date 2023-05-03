57904b45413ef766d3bfd64495e0c9fbd6fdf561
theduynguyen <theduynguyen2001@gmail.com>
2023-05-03 16:43:09 +1000

    global constants module added

---
+++ agent/constants.py
@@ -0,0 +1,7 @@
+from referee.game import PlayerColor
+
+INF            : float = 9999
+EMPTY_POWER    : int = 0
+MIN_MOVE_WIN   : int = 2
+PLAYER_COLOR   : PlayerColor = PlayerColor.RED
+OPPONENT_COLOR : PlayerColor = PLAYER_COLOR.opponent
