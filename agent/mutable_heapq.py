30351ba1d2e6889b79f718f1d89278c4cdaf7d9c
Raja Felipe <rfelipe@student.unimelb.edu.au>
2023-05-03 22:39:35 +1000

    MutableHeap: New Priority Queue that can update its nodes

---
+++ agent/mutable_heapq.py
@@ -0,0 +1,36 @@
+import heapq
+import itertools
+
+REMOVED = None
+NEW = 'new'
+
+
+class MutableHeap:
+    def __init__(self):
+        self.entry_finder = {}
+        self.pq = []
+        self.counter = itertools.counter()
+        return
+
+    def add_task(self, task, priority=0):
+        'Add a new task or update the priority of an existing task'
+        if task in self.entry_finder:
+            self.remove_task(task)
+        count = next(self.counter)
+        entry = [priority, count, task]
+        self.entry_finder[task] = entry
+        heapq.heappush(self.pq, entry)
+
+    def remove_task(self, task):
+        'Mark an existing task as REMOVED.  Raise KeyError if not found.'
+        entry = self.entry_finder.pop(task)
+        entry[-1] = REMOVED
+
+    def pop_task(self):
+        'Remove and return the lowest priority task. Raise KeyError if empty.'
+        while self.pq:
+            priority, count, task = heapq.heappop(self.pq)
+            if task is not REMOVED:
+                del self.entry_finder[task]
+                return task
+        raise KeyError('pop from an empty priority queue')
