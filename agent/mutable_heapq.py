5da38c31f70717cb5c6ee20e216b61ea80653018
Raja Felipe <rfelipe@student.unimelb.edu.au>
2023-05-04 01:16:49 +1000

    MUTABLEHEAP: Adjusted mutable heap for MonteCarlo nodes

--- agent/mutable_heapq.py
+++ agent/mutable_heapq.py
@@ -1,5 +1,7 @@
 import heapq
 import itertools
+from .mc_node import MonteCarloNode
+from agent.board import Board
 
 REMOVED = None
 NEW = 'new'
@@ -9,28 +11,28 @@ class MutableHeap:
     def __init__(self):
         self.entry_finder = {}
         self.pq = []
-        self.counter = itertools.counter()
+        self.counter = itertools.count()
         return
 
-    def add_task(self, task, priority=0):
+    def add_task(self, task: MonteCarloNode, priority=0):
         'Add a new task or update the priority of an existing task'
-        if task in self.entry_finder:
-            self.remove_task(task)
+        if task.hash_val in self.entry_finder:
+            self.remove_task(task.hash_val)
         count = next(self.counter)
         entry = [priority, count, task]
-        self.entry_finder[task] = entry
+        self.entry_finder[task.hash_val] = entry
         heapq.heappush(self.pq, entry)
 
-    def remove_task(self, task):
+    def remove_task(self, task: MonteCarloNode):
         'Mark an existing task as REMOVED.  Raise KeyError if not found.'
-        entry = self.entry_finder.pop(task)
-        entry[-1] = REMOVED
+        entry = self.entry_finder.pop(task.hash_val)
+        entry[-1] = None
 
     def pop_task(self):
         'Remove and return the lowest priority task. Raise KeyError if empty.'
         while self.pq:
             priority, count, task = heapq.heappop(self.pq)
-            if task is not REMOVED:
-                del self.entry_finder[task]
+            if task is not None:
+                del self.entry_finder[task.hash_val]
                 return task
         raise KeyError('pop from an empty priority queue')
