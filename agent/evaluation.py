24e14ae043d52783c059fda24c165d0e94fe1090
Raja Felipe <rfelipe@student.unimelb.edu.au>
2023-05-04 17:19:02 +1000

    EVALUATION: Made a new function for MonteCarlo Priority

--- agent/evaluation.py
+++ agent/evaluation.py
@@ -89,3 +89,71 @@ def evaluate(board: Board) -> (float, float):
     monte_carlo_val /= (num_opponent_clusters + num_opponent_dominates + pow_opponent_dominates)
 
     return value, monte_carlo_val
+
+def mc_evaluate(board: Board) -> float:
+    """
+    Evaluation function to evaluate the state of the board after a monte carlo simulation.
+    @param board : current state of board
+    @return      : the evaluated value of the board
+    """
+    # player power evaluation
+    num_blue, pow_blue = board.color_number_and_power(PlayerColor.BLUE)
+    num_red , pow_red  = board.color_number_and_power(PlayerColor.RED)
+    value  = (num_red - num_blue) * NUM_PIECE_FACTOR
+    value += (pow_red - pow_blue) * POWER_PIECE_FACTOR
+
+    # monte-carlo evaluation value
+    monte_carlo_val = 0
+
+    if num_blue == 0:
+        return INF, INF
+    if num_red == 0:
+        return -INF, -INF
+
+    # clusters and dominance evaluation
+    clusters = create_clusters(board)
+    num_player_clusters    = 0
+    num_player_dominates   = 0
+    pow_player_dominates   = 0
+    num_opponent_clusters  = 0
+    num_opponent_dominates = 0
+    pow_opponent_dominates = 0
+    for cluster in clusters.values():
+        sign = 1 if cluster.color == PlayerColor.RED else -1
+
+        # player's cluster
+        if cluster.color == PLAYER_COLOR:
+            num_player_clusters += 1
+            # dominance factor is checked solely via player pieces
+            for opponent in cluster.get_opponents():
+                opponent_cluster = clusters[opponent]
+
+                # cluster size dominance
+                if len(cluster) < len(opponent_cluster):
+                    num_opponent_dominates += sign
+                elif len(cluster) > len(opponent_cluster):
+                    num_player_dominates += sign
+
+                # cluster power dominance
+                if cluster.get_power() < opponent_cluster.get_power():
+                    pow_opponent_dominates += sign
+                elif cluster.get_power() > opponent_cluster.get_power():
+                    pow_player_dominates += sign
+
+        # opponent's cluster
+        else:
+            num_opponent_clusters += 1
+
+        # cluster and dominance factor add-on, respectively
+        value += sign * len(cluster) * SIZE_CLUSTER_FACTOR
+        value += (num_player_dominates - num_opponent_dominates) * SIZE_DOMINANCE_FACTOR
+        value += (pow_player_dominates - pow_opponent_dominates) * POWER_DOMINANCE_FACTOR
+    value += (num_player_clusters - num_opponent_clusters) * NUM_CLUSTER_FACTOR
+
+    # RAJA: Adding another return value specifically for ordering
+    # the heapq for MonteCarlo
+    player_val = num_player_clusters + num_player_dominates + pow_player_dominates
+    opponent_val = num_opponent_clusters + num_opponent_dominates + pow_opponent_dominates
+    monte_carlo_val = player_val/(player_val+opponent_val)
+
+    return monte_carlo_val
