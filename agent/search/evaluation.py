ab7fa31bc4393d6405eb17a0c0da9762a6a066be
theduynguyen <theduynguyen2001@gmail.com>
2023-05-05 15:46:32 +1000

    evaluation: property holder class EvaluateData implemented

--- agent/search/evaluation.py
+++ agent/search/evaluation.py
@@ -6,6 +6,7 @@
 The evaluation function will make use of information such as the number of pieces of a given side
 currently on the board, the total power, as well as their clusters.
 """
+from dataclasses import dataclass
 
 from referee.game import PlayerColor
 from agent.game import Board, PLAYER_COLOR, INF, create_clusters
@@ -19,6 +20,103 @@ NUM_DOMINANCE_FACTOR : float = 1.55
 POW_DOMINANCE_FACTOR : float = 0.45
 
 
+@dataclass(slots=True)
+class EvaluateData:
+    """
+    Class represents the information required for evaluating the board.
+    Attributes:
+        num_player             : the number of player pieces on the board
+        pow_player             : the player's total power
+        num_player_clusters    : the number of player's clusters
+        num_player_dominates   : the number of clusters where player dominates opponent
+        pow_player_dominates   : the power of clusters where player dominates opponent
+        size_player_clusters   : the size of player's clusters
+        num_opponent           : the number of opponent pieces on the board
+        pow_opponent           : the opponent's total power
+        num_opponent_clusters  : the number of opponent's clusters
+        num_opponent_dominates : the number of clusters where opponent dominates player
+        pow_opponent_dominates : the power of clusters where opponent dominates player
+        size_opponent_clusters : the size of player's clusters
+        sign                   : multiplier sign, if player is red then maximize by multiplying 1,
+                                 otherwise minimize by multiplying -1; only applicable for zero-sum
+        immediate_evaluation   : immediate evaluation value where the game is already over
+        immediate              : whether value is immediately evaluated or not
+    """
+    num_player             : int = 0
+    num_opponent           : int = 0
+    pow_player             : int = 0
+    pow_opponent           : int = 0
+    num_player_clusters    : int = 0
+    num_player_dominates   : int = 0
+    pow_player_dominates   : int = 0
+    size_player_clusters   : int = 0
+    num_opponent_clusters  : int = 0
+    num_opponent_dominates : int = 0
+    pow_opponent_dominates : int = 0
+    size_opponent_clusters : int = 0
+    sign                   : int = 1
+    immediate_evaluation   : int = 0
+    immediate              : bool = False
+
+
+def get_evaluate_data(board: Board) -> EvaluateData:
+    """
+    Get the data required for board evaluation.
+    @param board : current state of board
+    @return      : the evaluated value of the board
+    """
+    # number of pieces and total power evaluation data
+    sign      = 1 if PLAYER_COLOR == PlayerColor.RED else -1
+    data      = EvaluateData()
+    data.sign = sign
+    num_player, pow_player     = board.color_number_and_power(PLAYER_COLOR)
+    num_opponent, pow_opponent = board.color_number_and_power(PLAYER_COLOR.opponent)
+    data.num_player            = num_player
+    data.pow_player            = pow_player
+    data.num_opponent          = num_opponent
+    data.pow_opponent          = pow_opponent
+
+    # immediate evaluation data
+    if num_player == 0:
+        data.immediate_evaluation = -INF * sign
+        data.immediate = True
+        return data
+    elif num_opponent == 0:
+        data.immediate_evaluation = INF * sign
+        data.immediate = True
+        return data
+
+    # clusters and dominance evaluation data
+    clusters = create_clusters(board)
+    for cluster in clusters.values():
+
+        # player's cluster
+        if cluster.color == PLAYER_COLOR:
+            data.num_player_clusters  += 1
+            data.size_player_clusters += len(cluster)
+            # dominance factor is checked solely via player pieces
+            for opponent in cluster.get_opponents():
+                opponent_cluster = clusters[opponent]
+
+                # cluster size dominance
+                if len(cluster) < len(opponent_cluster):
+                    data.num_opponent_dominates += 1
+                elif len(cluster) > len(opponent_cluster):
+                    data.num_player_dominates += 1
+
+                # cluster power dominance
+                if cluster.get_power() < opponent_cluster.get_power():
+                    data.pow_opponent_dominates += 1
+                elif cluster.get_power() > opponent_cluster.get_power():
+                    data.pow_player_dominates += 1
+
+        # opponent's cluster
+        else:
+            data.num_opponent_clusters  += 1
+            data.size_opponent_clusters += len(cluster)
+    return data
+
+
 def evaluate(board: Board) -> float:
     """
     Evaluation function to evaluate the desirability of the board. It should be noted that the
@@ -28,125 +126,39 @@ def evaluate(board: Board) -> float:
     @param board : current state of board
     @return      : the evaluated value of the board
     """
-    # player power evaluation
-    num_blue, pow_blue = board.color_number_and_power(PlayerColor.BLUE)
-    num_red , pow_red  = board.color_number_and_power(PlayerColor.RED)
-    value  = (num_red - num_blue) * NUM_PIECE_FACTOR
-    value += (pow_red - pow_blue) * POW_PIECE_FACTOR
-
-    if num_blue == 0:
-        return INF
-    if num_red == 0:
-        return -INF
-
-    # clusters and dominance evaluation
-    clusters = create_clusters(board)
-    num_player_clusters    = 0
-    num_player_dominates   = 0
-    pow_player_dominates   = 0
-    size_player_clusters   = 0
-    num_opponent_clusters  = 0
-    num_opponent_dominates = 0
-    pow_opponent_dominates = 0
-    size_opponent_clusters = 0
-    for cluster in clusters.values():
-
-        # player's cluster
-        if cluster.color == PLAYER_COLOR:
-            num_player_clusters  += 1
-            size_player_clusters += len(cluster)
-            # dominance factor is checked solely via player pieces
-            for opponent in cluster.get_opponents():
-                opponent_cluster = clusters[opponent]
-
-                # cluster size dominance
-                if len(cluster) < len(opponent_cluster):
-                    num_opponent_dominates += 1
-                elif len(cluster) > len(opponent_cluster):
-                    num_player_dominates += 1
-
-                # cluster power dominance
-                if cluster.get_power() < opponent_cluster.get_power():
-                    pow_opponent_dominates += 1
-                elif cluster.get_power() > opponent_cluster.get_power():
-                    pow_player_dominates += 1
-
-        # opponent's cluster
-        else:
-            num_opponent_clusters  += 1
-            size_opponent_clusters += len(cluster)
-
-    # cluster and dominance factor add-on, respectively
-    sign = 1 if PLAYER_COLOR == PlayerColor.RED else -1
-    value += (size_player_clusters - size_opponent_clusters) * SIZE_CLUSTER_FACTOR
-    value += (num_player_dominates - num_opponent_dominates) * NUM_DOMINANCE_FACTOR
-    value += (pow_player_dominates - pow_opponent_dominates) * POW_DOMINANCE_FACTOR
-    value += (num_player_clusters  - num_opponent_clusters ) * NUM_CLUSTER_FACTOR
-    return value * sign
+    data: EvaluateData = get_evaluate_data(board)
+    if data.immediate:
+        return data.immediate_evaluation
+    value  = (data.num_player - data.num_opponent) * NUM_PIECE_FACTOR
+    value += (data.pow_player - data.pow_opponent) * POW_PIECE_FACTOR
+    value += (data.size_player_clusters - data.size_opponent_clusters) * SIZE_CLUSTER_FACTOR
+    value += (data.num_player_dominates - data.num_opponent_dominates) * NUM_DOMINANCE_FACTOR
+    value += (data.pow_player_dominates - data.pow_opponent_dominates) * POW_DOMINANCE_FACTOR
+    value += (data.num_player_clusters  - data.num_opponent_clusters ) * NUM_CLUSTER_FACTOR
+    value *= data.sign
+    return value
 
 
 def mc_evaluate(board: Board) -> float:
     """
-    Evaluation function to evaluate the state of the board after a monte carlo simulation.
-    This is not a zero-sum evaluation function, but an evaluation function for the simulation.
+    Evaluation function to evaluate the state of the board after a monte carlo simulation. This
+    is not a zero-sum evaluation function, but an evaluation function for the simulation. The
+    evaluation is normalized, meaning if it is a win for the player, it will be 1, and if it is
+    definite loss then it will be 0. Any value in between shows the desirability of the state.
     @param board : current state of board
     @return      : the evaluated value of the board
     """
-    # player power evaluation
-    num_blue, pow_blue = board.color_number_and_power(PlayerColor.BLUE)
-    num_red , pow_red  = board.color_number_and_power(PlayerColor.RED)
-    value  = (num_red - num_blue) * NUM_PIECE_FACTOR
-    value += (pow_red - pow_blue) * POW_PIECE_FACTOR
-
-    if num_blue == 0:
-        return INF
-    if num_red == 0:
-        return -INF
-
-    # clusters and dominance evaluation
-    clusters = create_clusters(board)
-    num_player_clusters    = 0
-    num_player_dominates   = 0
-    pow_player_dominates   = 0
-    size_player_clusters   = 0
-    num_opponent_clusters  = 0
-    num_opponent_dominates = 0
-    pow_opponent_dominates = 0
-    size_opponent_clusters = 0
-    for cluster in clusters.values():
-
-        # player's cluster
-        if cluster.color == PLAYER_COLOR:
-            num_player_clusters  += 1
-            size_player_clusters += len(cluster)
-            # dominance factor is checked solely via player pieces
-            for opponent in cluster.get_opponents():
-                opponent_cluster = clusters[opponent]
-
-                # cluster size dominance
-                if len(cluster) < len(opponent_cluster):
-                    num_opponent_dominates += 1
-                elif len(cluster) > len(opponent_cluster):
-                    num_player_dominates += 1
-
-                # cluster power dominance
-                if cluster.get_power() < opponent_cluster.get_power():
-                    pow_opponent_dominates += 1
-                elif cluster.get_power() > opponent_cluster.get_power():
-                    pow_player_dominates += 1
-
-        # opponent's cluster
-        else:
-            num_opponent_clusters  += 1
-            size_opponent_clusters += len(cluster)
+    data: EvaluateData = get_evaluate_data(board)
+    if data.immediate:
+        return data.immediate_evaluation
 
     # adding another return value specifically for ordering the heapq for MonteCarlo
-    player_val   = num_player_clusters    * NUM_CLUSTER_FACTOR   + \
-                   size_player_clusters   * SIZE_CLUSTER_FACTOR  + \
-                   num_player_dominates   * NUM_DOMINANCE_FACTOR + \
-                   pow_player_dominates   * POW_DOMINANCE_FACTOR
-    opponent_val = num_opponent_clusters  * NUM_CLUSTER_FACTOR   + \
-                   size_opponent_clusters * SIZE_CLUSTER_FACTOR  + \
-                   num_opponent_dominates * NUM_DOMINANCE_FACTOR + \
-                   pow_opponent_dominates * POW_DOMINANCE_FACTOR
+    player_val   = data.num_player_clusters    * NUM_CLUSTER_FACTOR   + \
+                   data.size_player_clusters   * SIZE_CLUSTER_FACTOR  + \
+                   data.num_player_dominates   * NUM_DOMINANCE_FACTOR + \
+                   data.pow_player_dominates   * POW_DOMINANCE_FACTOR
+    opponent_val = data.num_opponent_clusters  * NUM_CLUSTER_FACTOR   + \
+                   data.size_opponent_clusters * SIZE_CLUSTER_FACTOR  + \
+                   data.num_opponent_dominates * NUM_DOMINANCE_FACTOR + \
+                   data.pow_opponent_dominates * POW_DOMINANCE_FACTOR
     return player_val / (player_val + opponent_val)
