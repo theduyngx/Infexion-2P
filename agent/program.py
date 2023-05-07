faae0e74cbcd950aec09bb1fa3dc3b68fcb64bec
theduynguyen <theduynguyen2001@gmail.com>
2023-05-07 15:52:33 +1000

    agent: simplified for submission

--- agent/program.py
+++ agent/program.py
@@ -9,14 +9,13 @@ Purpose:
 Notes:
     COMP30024 Artificial Intelligence, Semester 1 2023 - Project Part B: Game Playing Agent. When
     mentioning the rules by referee, it is important to note the following functions:
-    ``action`` to return an action that the agent decides to perform, and
-    ``turn`` which will be called as a signal for agent that it is their turn.
+        * ``action`` to return an action that the agent decides to perform, and
+        * ``turn`` which will be called as a signal for agent that it is their turn.
 """
 
 from .search import search
 from .search.agent_test import *
 from .game import Board
-from .utils import *
 
 
 class Agent:
@@ -39,7 +38,7 @@ class Agent:
             color     : the player's color
             **referee : the referee containing specific important information of the game
         """
-        print_referee(referee)
+        # print_referee(referee)
         self._color = color
         self._board = Board()
 
@@ -64,25 +63,25 @@ class Agent:
         """
         board = self._board
         color = self._color
-        color_print = ansi_color(color)
-        print(f"{color_print} TURN:")
-        print_referee(referee)
-        # return search(board, color)
+        # color_print = ansi_color(color)
+        # print(f"{color_print} TURN:")
+        # print_referee(referee)
+        return search(board, color)
 
-        match color:
-            case PlayerColor.RED:
-                # return search(board, color)
-                return greedy_move(board, color)
-
-            case PlayerColor.BLUE:
-                # return mcts_move(board)
-                # return search(board, color)
-                # return minimax_shallow(board, color)
-                # return random_move(board, color)
-                # return greedy_move(board, color)
-                return search(board, color)
-            case _:
-                raise Exception(f"{color} is not of proper PlayerColor type")
+        # match color:
+        #     case PlayerColor.RED:
+        #         # return search(board, color)
+        #         return greedy_move(board, color)
+        #
+        #     case PlayerColor.BLUE:
+        #         # return mcts_move(board)
+        #         # return search(board, color)
+        #         # return minimax_shallow(board, color)
+        #         # return random_move(board, color)
+        #         # return greedy_move(board, color)
+        #         return search(board, color)
+        #     case _:
+        #         raise Exception(f"{color} is not of proper PlayerColor type")
 
     def turn(self, color: PlayerColor, action: Action, **referee: dict):
         """
@@ -94,14 +93,14 @@ class Agent:
             action  : the action taken by agent
             referee : the referee
         """
-        assert referee
+        # assert referee
         self._board.apply_action(action)
-        color_print = ansi_color(color)
-
-        match action:
-            case SpawnAction(cell):
-                print(f"{color_print} SPAWN at {cell}")
-                pass
-            case SpreadAction(cell, direction):
-                print(f"{color_print} SPREAD from {cell} - {direction}")
-                pass
+        # color_print = ansi_color(color)
+        #
+        # match action:
+        #     case SpawnAction(cell):
+        #         print(f"{color_print} SPAWN at {cell}")
+        #         pass
+        #     case SpreadAction(cell, direction):
+        #         print(f"{color_print} SPREAD from {cell} - {direction}")
+        #         pass
